d5f81f04458fd445456303a1b1ecf7af87112203
Rezwan66 <shaikhrezwan66@gmail.com>
2023-09-21 00:52:09 +0600

    handled going back to products page from product details page using useNavigate

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,16 +1,31 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const ProductDetails = () => {
   const productDetails = useLoaderData();
   //   console.log(productDetails);
   const { brand, category, description, images, price, title } = productDetails;
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div>
       <div className="grid gap-4">
         <div className="flex justify-between">
           <img className="h-80 max-w-full rounded-lg" src={images[0]} alt="" />
-          <h1>{brand}</h1>
-          <h2>{title}</h2>
+          <div className="w-[60%] flex gap-40 items-center">
+            <div className="space-y-4">
+              <h1 className="text-2xl font-medium">{brand}</h1>
+              <h2 className="text-5xl font-bold">{title}</h2>
+              <i>#{category}</i>
+              <p>{description}</p>
+            </div>
+            <div>
+              <strong className="text-8xl">${price}</strong>
+            </div>
+          </div>
         </div>
         <div className="grid grid-cols-4 gap-4">
           <div>
@@ -42,6 +57,15 @@ const ProductDetails = () => {
             />
           </div>
         </div>
+      </div>
+      <div className="mt-24">
+        <button
+          onClick={handleGoBack}
+          type="button"
+          className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full"
+        >
+          Go Back
+        </button>
       </div>
     </div>
   );
